478885402ed90a9ef171d41dea6d2a883cdc1c1c
Shannon Brooks <shannon@brooksworks.com>
2011-06-26 03:47:29 -0500

    so I just can't spell...

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,7 @@
  * Redirector Plugin for WolfCMS <http://www.wolfcms.org>
  * Copyright (c) 2011 Shannon Brooks <shannon@brooksworks.com>
  * Adapted from Redirector by Design Spike <http://designspike.ca>
- * Major contributions and original idea by Cody at Desgin Spike
+ * Major contributions and original idea by Cody at Design Spike
  *
  * Licensed under the MIT license
  * http://www.opensource.org/licenses/mit-license.php
