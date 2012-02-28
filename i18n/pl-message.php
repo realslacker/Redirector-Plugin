d8babb2a09a8aafac045b18970368a650d149827
marekmurawski <marek.murawski@gmail.com>
2012-02-28 15:21:55 +0100

    Initial polish translation for 0.2.5

---
+++ i18n/pl-message.php
@@ -0,0 +1,68 @@
+<?php
+
+    /**
+     * Polish pl-PL translation file for redirector plugin
+     * @author Marek Murawski <http://marekmurawski.pl>
+     * @version 0.2.5
+     *
+     * Redirector Plugin for WolfCMS <http://www.wolfcms.org>
+     * Copyright (c) 2011 Shannon Brooks <shannon@brooksworks.com>
+     * Adapted from Redirector by Design Spike <http://designspike.ca>
+     * Major contributions and original idea by Cody at Design Spike
+     *
+     * Licensed under the MIT license
+     * http://www.opensource.org/licenses/mit-license.php
+     *
+     * Project home
+     * http://www.github.com/realslacker/Redirector-Plugin
+     * 
+     */
+
+    return array(
+        '404 Errors have been cleared!' => 'Usunięto listę błędów 404',
+        '404 Errors' => 'Błędy 404',
+        'Active Redirects' => 'Aktywne przekierowania',
+        'Add Redirect' => 'Dodaj Alias',
+        'Could not alter table 1 of 2.' => 'Nie można było zmodyfikować tabeli 1 z 2.',
+        'Could not alter table 2 of 2.' => 'Nie można było zmodyfikować tabeli 2 z 2.',
+        'Could not create table 1 of 2.' => 'Nie można było utworzyć tabeli 1 z 2.',
+        'Could not create table 2 of 2.' => 'Nie można było utworzyć tabeli 2 z 2.',
+        'Could not find redirect!' => 'Nie znaleziono przekierowania',
+        'Could not remove plugin settings.' => 'Nie można było usunąć ustawień wtyczki.',
+        'Could not remove table 1 of 2.' => 'Nie można było usunąć tabeli 1 z 2.',
+        'Could not remove table 2 of 2.' => 'Nie można było usunąć tabeli 1 z 2.',
+        'Could not save settings!' => 'Nie można zapisać ustawień!',
+        'Days after last click before redirect automatically expires. Enter 0 to disable.' => 'Dni po ostatnim dostępie, gdy przekierowanie automatycznie wygasa. Wpisz 0 aby wyłączyć automatyczne wygasanie.',
+        'Expire After:' => 'Wygasa po:',
+        'General Configuration' => 'Konfiguracja ogólna',
+        'Hits' => 'Trafienia',
+        'Legacy version set. Please re-enable plugin.' => 'Aktywowano wersję zgodności. Aktywuj wtyczkę ponownie.',
+        'Number of times a page has to 404 to show up in redirector.' => 'Ile razy musi nastąpić błąd 404, aby wyświetlić go na liście.',
+        'Only MySQL and SQLite are supported!' => 'Tylko mySQL i SQLite są obsługiwane!',
+        'Plugin Version' => 'Wersja wtyczki',
+        'Redirect has been added!' => 'Przekierowanie zostało dodane!',
+        'Redirect has been deleted!' => 'Usunięto przekierowanie!',
+        'Redirector Plugin Settings' => 'Ustawienia wtyczki przekierowań',
+        'Redirector Plugin' => 'Wtyczka Przekierowań',
+        'Redirector' => 'Aliasy',
+        'Redirects To' => 'Przekierowuje do',
+        'Requested URL' => 'Ządany adres URL',
+        'Save' => 'Zapisz',
+        'Successfully installed Redirector plugin.' => 'Pomyślnie zainstalowano wtyczkę przekierowań.',
+        'Successfully uninstalled plugin.' => 'Pomyślnie odinstalowano wtyczkę przekierowań.',
+        'Successfully upgraded Redirector plugin.' => 'Pomyślnie zaktualizowano wtyczkę przekierowań.',
+        'There are no 404 errors yet.' => 'Nie ma jeszcze błędów 404.',
+        'There are no redirects set up yet.' => 'Nie utworzono jeszcze przekierowań',
+        'There was a problem adding your redirect.' => 'Napotkano problem przy dodawaniu przekierowania!',
+        'There was a problem clearing the 404 errors!' => 'Napotkano problem przy czyszczeniu listy błędów 404!',
+        'Threshold:' => 'Próg:',
+        'Unable to retrieve plugin settings.' => 'Nie można odczytać ustawień wtyczki.',
+        'Unable to store legacy version!' => 'Nie można zapisać wersji zgodności!',
+        'Unable to store plugin settings!' => 'Nie można zapisać ustawień wtyczki!',
+        'Unknown Version!' => 'Nieznana wersja',
+        'unlocked' => 'odblokowano',
+        'unlocking' => 'odblokowywanie',
+        'You do not have permission to access the requested page!' => 'Nie masz uprawnień dostępu do żądanej strony!',
+        'You must specify a destination url!' => 'Musisz podać URL docelowy!',
+        'You must specify a url!' => 'Musisz podać URL!',
+    );
